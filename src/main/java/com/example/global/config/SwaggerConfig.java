8e66850b551b3ee6ad3807d44ef946f53ceb9eb9
asa9874 <pjkpjk1129@gmail.com>
2025-05-12 21:57:22 +0900

    Feat: GlobalExceptionHandler 클래스 추가, logback.xml 설정

--- src/main/java/com/example/global/config/SwaggerConfig.java
+++ src/main/java/com/example/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.config;
+package com.example.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/example/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.example.global.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import io.swagger.v3.oas.annotations.Hidden;
+/*
+ * 컨트롤러 수준의 예외를 처리하는 클래스
+ * 반환으로 ResponseEntity를 사용하여 HTTP 상태 코드와 메시지를 반환
+ */
+@Hidden
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleUnexpectedError(Exception ex) {
+        log.error("Unexpected error: {}", ex.getMessage());
+        String apiResponse = "Unexpected error: " + ex.getMessage();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiResponse);
+    }   
+}
