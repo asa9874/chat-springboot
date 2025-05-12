c0b4af0d7537b117cd0a4de78e69396deca97829
asa9874 <pjkpjk1129@gmail.com>
2025-05-12 21:57:42 +0900

    Fix: Member import 수정

--- src/main/java/com/example/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/domain/member/repository/MemberRepository.java
@@ -1,8 +1,9 @@
 package com.example.domain.member.repository;
 
-import java.lang.reflect.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.domain.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
